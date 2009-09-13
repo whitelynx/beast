abc86a4393dcc5d7fc4e5df229f9bf344c9042c9
Stefan Westerfeld <stefan@space.twc.de>
2009-09-13 16:59:15 +0200

    Use a portable way of figuring out temp dir (which is not always /tmp).
    Based on 013_doxer_portability.diff.

--- doxer/Config.py
+++ doxer/Config.py
@@ -17,11 +17,12 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 import sys
+import tempfile
 
 # --- constants ---
 init_dict = {
   'CONFIG_VERSION'      : 0.6,
-  'TMP_DIR'		: '/tmp',
+  'TMP_DIR'		: tempfile.gettempdir(),
 }
 
 # --- constant dictionary ---
